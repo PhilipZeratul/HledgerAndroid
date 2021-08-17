a78d3deb35dc362fe4735ba107d189bd1114e456
Phillip Zeratul <p.f.zeratul@gmail.com>
2021-08-17 17:45:02 +0800

    Read in common.journal

--- app/src/main/java/com/phillip/hledgerandroid/MainActivity.java
+++ app/src/main/java/com/phillip/hledgerandroid/MainActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    private static final String TAG = "HledgerAndroid";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_load_accounts:
-
+                loadAccounts();
                 return true;
             case R.id.action_save_csv:
 
@@ -52,15 +59,39 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
     }
 
     // TODO: Read in common.journal to know what accounts do I have.
-    private void openFile(Uri pickerInitialUri) {
+    private static final int LOAD_ACCOUNT = 2;
+
+    private void loadAccounts() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("text/plain");
+        intent.setType("*/*");
+        startActivityForResult(intent, LOAD_ACCOUNT);
+    }
 
-        // Optionally, specify a URI for the file that should appear in the
-        // system file picker when it loads.
-        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        if (requestCode == LOAD_ACCOUNT && resultCode == Activity.RESULT_OK) {
+            // The result data contains a URI for the document or directory that
+            // the user selected.
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                // Perform operations on the document using its URI.
+                try {
+                    InputStream in = getContentResolver().openInputStream(uri);
+                    BufferedReader r = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder total = new StringBuilder();
+                    for (String line; (line = r.readLine()) != null; ) {
+                        total.append(line).append('\n');
+                    }
 
-        startActivityForResult(intent, 2);
+                    String content = total.toString();
+                    Log.v(TAG, content);
+                }catch (Exception e) {
+
+                }
+            }
+        }
     }
 }
